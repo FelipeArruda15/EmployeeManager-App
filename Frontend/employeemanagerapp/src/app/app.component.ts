4450c2752ebeb948d57e766173880c1684e68789
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-04-27 20:11:02 -0300

    Configurando servicos e criando a URL do backend

--- Frontend/employeemanagerapp/src/app/app.component.ts
+++ Frontend/employeemanagerapp/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Funcionario } from './funcionario';
+import { FuncionarioService } from './funcionario.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeemanagerapp';
+export class AppComponent implements OnInit {
+  public funcionarios: Funcionario[] = [];
+
+  constructor(private service: FuncionarioService) { }
+
+  ngOnInit(): void {
+    this.obterFuncionarios();
+  }
+
+  public obterFuncionarios(): void {
+    this.service.obterFuncionarios().subscribe(
+      (resposta: Funcionario[]) => {
+        this.funcionarios = resposta;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    );
+  }
+
 }
--- Frontend/employeemanagerapp/src/app/app.module.ts
+++ Frontend/employeemanagerapp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { FuncionarioService } from './funcionario.service';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [FuncionarioService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
